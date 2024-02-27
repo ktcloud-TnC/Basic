84eab001816931730908809f2de963d0c91d43be
ktcloud-TnC <ktcloud.edu@gmail.com>
2024-02-27 18:51:08 +0900

    무브 수정

--- web/move.sh
+++ web/move.sh
@@ -4,6 +4,6 @@
 
 # mv project01.service /etc/systemd/system
 
-mv /root/Basic/project01 /var/www/
+mv /root/Basic/web/project01 /var/www/
 
-mv /root/project01.service /etc/systemd/system
+mv /root/Basic/web/project01.service /etc/systemd/system
