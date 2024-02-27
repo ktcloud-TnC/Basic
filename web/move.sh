defca6ba1661efc835b0933a8b91f8dbe8c72e9f
ktcloud-TnC <ktcloud.edu@gmail.com>
2024-02-27 17:49:13 +0900

    update

--- web/move.sh
+++ web/move.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# mv ./project01 /var/wwww/
+# mv ./project01 /var/www/
 
 # mv project01.service /etc/systemd/system
 
-mv /home/ubuntu/Basic/project01 /var/wwww/
+mv /home/ubuntu/Basic/project01 /var/www/
 
 mv /home/ubuntu/Basic/project01.service /etc/systemd/system
